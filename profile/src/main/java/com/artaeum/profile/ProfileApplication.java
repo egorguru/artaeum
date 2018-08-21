5f310e425adc7df8ba75302a6a905cae5884405f
EgorTrevelyan <egorepnikov@gmail.com>
2018-08-21 22:24:40 +0300

    Add Profile Service

---
+++ profile/src/main/java/com/artaeum/profile/ProfileApplication.java
@@ -0,0 +1,18 @@
+package com.artaeum.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableOAuth2Client
+@EnableFeignClients
+public class ProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileApplication.class, args);
+    }
+}
---
+++ profile/src/main/java/com/artaeum/profile/config/ResourceServerConfig.java
@@ -0,0 +1,31 @@
+package com.artaeum.profile.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .headers()
+                .frameOptions()
+                .disable()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/profile/**").permitAll()
+                .antMatchers("/**").authenticated();
+    }
+}
