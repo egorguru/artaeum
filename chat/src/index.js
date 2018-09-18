a1fda6cf577ab7e4fa19d28995ee61ef324177e6
EgorTrevelyan <egorepnikov@gmail.com>
2018-09-18 12:37:48 +0300

    Remove Chat service (It will be implemented in the future)

--- chat/src/index.js
+++
@@ -1,8 +0,0 @@
-import app from './app';
-import config from './cloud/config-client';
-
-config.then((c) => {
-  app.listen(parseInt(c.get('server.port')), () => {
-    console.log(`Server has been started on port ${c.get('server.port')}`)
-  })
-})
