a82a0c20e8321fedd77f7062dc005c9b2ad5d38c
EgorTrevelyan <egorepnikov@gmail.com>
2018-10-04 14:12:41 +0300

    Add Health api to Comment service

--- comment/src/controllers/index.js
+++ comment/src/controllers/index.js
@@ -1,9 +1,10 @@
 const Router = require('koa-router')
 
 const comments = require('./comment')
+const health = require('./health')
 
 const router = new Router().prefix('/comment')
 
-router.use(comments)
+router.use(comments, health)
 
 module.exports = router
