4b39bdbe9cc771d178f40e45e664d79b04fed6af
EgorTrevelyan <egorepnikov@gmail.com>
2018-10-16 14:35:28 +0300

    Create Footer's template

--- web-client/src/app/layouts/footer/footer.component.ts
+++ web-client/src/app/layouts/footer/footer.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'ae-footer',
-    templateUrl: './footer.component.html'
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {}
