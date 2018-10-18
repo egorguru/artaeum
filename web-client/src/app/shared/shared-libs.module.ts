f9f36dc6009406a83678066632170d287e39f1e5
EgorTrevelyan <egorepnikov@gmail.com>
2018-10-18 16:51:36 +0300

    Fix dev server, ngIf directives and export CommonModule

--- web-client/src/app/shared/shared-libs.module.ts
+++ web-client/src/app/shared/shared-libs.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core'
 import { HttpClientModule } from '@angular/common/http'
 import { ReactiveFormsModule } from '@angular/forms'
+import { CommonModule } from '@angular/common'
 
 @NgModule({
   exports: [
+    CommonModule,
     HttpClientModule,
     ReactiveFormsModule
   ]
