c6e8684284ab90a58903e0d71275c435b99d47a5
EgorTrevelyan <egorepnikov@gmail.com>
2018-10-19 19:04:18 +0300

    Fix falling builds

--- web-client/src/environments/environment.prod.ts
+++ web-client/src/environments/environment.prod.ts
@@ -1,4 +1,15 @@
 export const environment = {
   production: true,
-  SERVER_API_URL: 'http://localhost:8080'
+  SERVER_API_URL: 'http://localhost:8080',
+  POSTS_PER_PAGE: 10,
+  MOUNTHS_IN_ENGLISH: [
+    'January', 'February', 'March', 'April',
+    'May', 'June', 'July', 'August',
+    'September', 'October', 'November', 'December'
+  ],
+  MONTHS_IN_RUSSIAN: [
+    'Январь', 'Февраль', 'Март', 'Апрель',
+    'Май', 'Июнь', 'Июль', 'Август',
+    'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'
+  ]
 }
