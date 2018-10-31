782e0a3f86ed2d670d0f41132fa2a7e0386ee18f
EgorTrevelyan <egorepnikov@gmail.com>
2018-10-31 12:53:49 +0300

    Add guard to Author routes

--- web-client/src/app/author/author.route.ts
+++ web-client/src/app/author/author.route.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router'
+import { UserRouteAccessService } from '../shared'
 
 import { createUpdateBlogPostRoutes } from './create-blog-post'
 
 export const authorRoutes: Routes = [{
   path: 'author',
+  data: {
+    authorities: ['user']
+  },
+  canActivate: [UserRouteAccessService],
   children: [
     ...createUpdateBlogPostRoutes
   ]
