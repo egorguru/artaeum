b979109c9468ae16202f656b43d0bcc1a1074fc3
EgorTrevelyan <egorepnikov@gmail.com>
2018-12-01 17:58:23 +0300

    Add change language feature

--- web-client/src/app/layouts/footer/footer.component.ts
+++ web-client/src/app/layouts/footer/footer.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { TranslateService } from '@ngx-translate/core'
 
 @Component({
-    selector: 'ae-footer',
-    templateUrl: './footer.component.html',
-    styleUrls: ['./footer.component.css']
+  selector: 'ae-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {}
+export class FooterComponent implements OnInit {
+
+  languages = {
+    en: '🇺🇸',
+    ru: '🇷🇺'
+  }
+
+  currentLang: string
+
+  constructor(private translateService: TranslateService) {}
+
+  ngOnInit() {
+    this.currentLang = this.translateService.defaultLang
+    this.translateService.onLangChange.subscribe((event) => {
+      this.currentLang = event.lang
+    })
+  }
+
+  changeLanguage(lang: string) {
+    this.translateService.use(lang)
+  }
+}
--- web-client/src/app/layouts/main/main.component.ts
+++ web-client/src/app/layouts/main/main.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { TranslateService } from '@ngx-translate/core'
+
+import { Principal } from '../../shared'
 
 @Component({
   selector: 'ae-main',
   templateUrl: './main.component.html'
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
 
-  constructor(translate: TranslateService) {
-    translate.setDefaultLang('en')
+  constructor(
+    private translateService: TranslateService,
+    private principal: Principal
+  ) {
+    translateService.setDefaultLang('en')
+  }
+
+  ngOnInit() {
+    this.principal.identity().then((u) => {
+      this.translateService.use(u.langKey || 'en')
+    })
   }
 }
