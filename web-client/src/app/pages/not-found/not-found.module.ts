8ddfda9b38a64b65db776f03f1e71a30287dbede
EgorTrevelyan <egorepnikov@gmail.com>
2018-12-02 19:45:07 +0300

    Fix import in NotFound module

--- web-client/src/app/pages/not-found/not-found.module.ts
+++ web-client/src/app/pages/not-found/not-found.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
 
-import { SharedModule } from 'src/app/shared'
+import { SharedModule } from '../../shared'
 import { NotFoundComponent } from './not-found.component'
 import { routes } from './not-found.route'
 
