a31e9d1b1f6cb42360969e62dfbf9fc256bef484
EgorTrevelyan <egorepnikov@gmail.com>
2018-12-12 15:16:09 +0300

    Remove CORS filter from security and add global CORS config

--- gateway/src/main/java/com/artaeum/gateway/config/ResourceServerConfig.java
+++ gateway/src/main/java/com/artaeum/gateway/config/ResourceServerConfig.java
@@ -7,13 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableResourceServer
@@ -25,7 +19,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         http
                 .csrf()
                     .disable()
-                    .addFilterBefore(corsFilter(), CsrfFilter.class)
                     .headers()
                     .frameOptions()
                     .disable()
@@ -35,20 +28,6 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                 .and()
                     .authorizeRequests()
                     .antMatchers("/**").permitAll();
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("*"));
-        config.setAllowedMethods(Arrays.asList("*"));
-        config.setAllowedHeaders(Arrays.asList("*"));
-        config.setExposedHeaders(Arrays.asList("Authorization", "X-Total-Count"));
-        config.setAllowCredentials(true);
-        config.setMaxAge(3600L);
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
     }
 
     @Bean
