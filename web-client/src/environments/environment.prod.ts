412f7af841bcf97b24e701d876bfc02d9aeef1a9
EgorTrevelyan <egorepnikov@gmail.com>
2018-12-14 14:56:26 +0300

    Edit configs

--- web-client/src/environments/environment.prod.ts
+++ web-client/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  SERVER_API_URL: 'http://localhost:8080',
-  IMAGE_BASE_URL: 'http://localhost:8080/storage/images/',
+  SERVER_API_URL: 'https://ae-gateway.herokuapp.com',
+  IMAGE_BASE_URL: 'https://ae-gateway.herokuapp.com/storage/images/',
   POSTS_PER_PAGE: 10,
   MONTHS: {
     en: [
