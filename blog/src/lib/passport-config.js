ce65d997246c4b9e6adc856b31fc8accfaa7b14d
EgorTrevelyan <egorepnikov@gmail.com>
2018-12-29 18:25:14 +0300

    Edit configs

--- blog/src/lib/passport-config.js
+++ blog/src/lib/passport-config.js
@@ -1,12 +1,10 @@
 const BearerStrategy = require('passport-http-bearer')
 const rp = require('request-promise')
 
-const config = require('./config-client')
-
-function initBearerStrategy(config) {
+function initBearerStrategy() {
   return new BearerStrategy((token, done) => {
     rp({
-      uri: process.env[config.get('security.oauth2.resource.user-info-uri')] || 'http://uaa:5000/uaa/account/current',
+      uri: process.env.OAUTH2_USER_INFO_URI || 'http://uaa:5000/uaa/account/current',
       headers: { 'Authorization': `Bearer ${token}` },
       json: true
     }).then((data) => {
@@ -18,9 +16,7 @@ function initBearerStrategy(config) {
 }
 
 module.exports = (passport) => {
-  config.then((config) => {
-    passport.use(initBearerStrategy(config))
-    passport.serializeUser((user, done) => done(null, user))
-    passport.deserializeUser((user, done) => done(null, user))
-  })
+  passport.use(initBearerStrategy())
+  passport.serializeUser((user, done) => done(null, user))
+  passport.deserializeUser((user, done) => done(null, user))
 }
