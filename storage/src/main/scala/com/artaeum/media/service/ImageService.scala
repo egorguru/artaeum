ab9793ebe19617b7bfa5a46900d8ce5a451e012b
EgorTrevelyan <egorepnikov@gmail.com>
2019-01-15 15:15:45 +0300

    Add image compressing to server side (Storage service)

--- storage/src/main/scala/com/artaeum/media/service/ImageService.scala
+++ storage/src/main/scala/com/artaeum/media/service/ImageService.scala
@@ -1,12 +1,13 @@
 package com.artaeum.media.service
 
-import java.io.{ByteArrayInputStream, File}
+import java.io.{ByteArrayInputStream, File, FileOutputStream}
+import java.net.URI
 import java.nio.file.{Files, Paths}
 import java.util.Base64
 
 import com.artaeum.media.config.Constants
 import javax.annotation.PostConstruct
-import javax.imageio.ImageIO
+import javax.imageio.{IIOImage, ImageIO, ImageWriteParam}
 import org.springframework.core.env.Environment
 import org.springframework.stereotype.Service
 
@@ -25,7 +26,27 @@ class ImageService(env: Environment) {
     val path = Paths.get(resource, String.format("%s.%s", name, Constants.DEFAULT_IMAGE_EXPANSION)).toUri
     val bytes = Base64.getDecoder.decode(image.split(",")(1))
     ImageIO.write(ImageIO.read(new ByteArrayInputStream(bytes)), Constants.DEFAULT_IMAGE_EXPANSION, new File(path))
+    this.compress(path)
   }
 
   def delete(resource: String, name: String): Unit = Files.delete(Paths.get(resource, name))
+
+  private def compress(uri: URI): Unit = {
+    val image = new File(uri)
+    val bufferedImage = ImageIO.read(image)
+
+    val fos = new FileOutputStream(image)
+    val ios = ImageIO.createImageOutputStream(fos)
+    val writer = ImageIO.getImageWritersByFormatName(Constants.DEFAULT_IMAGE_EXPANSION).next
+    writer.setOutput(ios)
+
+    val param = writer.getDefaultWriteParam
+    param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT)
+    param.setCompressionQuality(0.5f)
+
+    writer.write(null, new IIOImage(bufferedImage, null, null), param)
+
+    fos.close()
+    ios.close()
+  }
 }
