e58e712971f1882008eb7ed0034c71e67a59fc1e
EgorTrevelyan <egorepnikov@gmail.com>
2019-01-21 19:13:37 +0300

    Refactor Comment's tests

--- comment/src/tests/controllers/comment.test.js
+++ comment/src/tests/controllers/comment.test.js
@@ -9,12 +9,6 @@ describe("Comments API", () => {
     userId: 'uuid-test',
     createdDate: Date.now()
   }
-  before(async () => {
-    await helpers.before()
-  })
-  after(async () => {
-    await helpers.after()
-  })
   beforeEach(async () => {
     await Comment.remove()
   })
