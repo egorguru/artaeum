898e03444bc58ebdbeee5f37805c79ca0b47b817
EgorTrevelyan <egorepnikov@gmail.com>
2019-02-15 16:00:09 +0300

    Add current IP to Stats request

--- web-client/src/app/layouts/shared/service/statistics.service.ts
+++ web-client/src/app/layouts/shared/service/statistics.service.ts
@@ -1,24 +1,44 @@
 import { Injectable } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpBackend } from '@angular/common/http'
 import { Router, NavigationEnd } from '@angular/router'
 
 @Injectable({ providedIn: 'root' })
 export class StatisticsService {
 
+  ip: string
+
   constructor(
     private http: HttpClient,
-    private router: Router
+    private router: Router,
+    private httpBackend: HttpBackend
   ) {}
 
   init(): void {
-    if (typeof (location) !== 'undefined') {
-      this.router.events.subscribe((v) => {
-        if (v instanceof NavigationEnd) {
-          this.http.post('statistics/stats', {
-            url: location.href
-          }).subscribe()
+    this.router.events.subscribe((v) => {
+      if (v instanceof NavigationEnd) {
+        if (!this.ip) {
+          this.loadIp().subscribe((res) => {
+            this.ip = res.ip
+            this.sendStats()
+          })
+        } else {
+          this.sendStats()
         }
-      })
+      }
+    })
+  }
+
+  private loadIp() {
+    const clearHttpClient = new HttpClient(this.httpBackend)
+    return clearHttpClient.get<{ ip: string }>('https://api.ipify.org?format=json')
+  }
+
+  private sendStats(): void {
+    if (typeof (location) !== 'undefined') {
+      this.http.post('statistics/stats', {
+        ip: this.ip,
+        url: location.href
+      }).subscribe()
     }
   }
 }
