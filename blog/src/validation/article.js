93f680221ec80057e3fed2c30c76f0db8fd8330e
EgorTrevelyan <egorepnikov@gmail.com>
2019-04-19 20:41:05 +0300

    Refactor Article validation

--- blog/src/validation/article.js
+++ blog/src/validation/article.js
@@ -1,17 +1,19 @@
 const { isMongoId, isLength, isBase64 } = require('validator')
 
+const isNotEmpty = require('../validation/is-not-empty-string')
+
 exports.create = (title, body, image, category) => (
-  isLength(title, { min: 3, max: 100 }) &&
-  isLength(body, { min: 10 }) &&
+  isNotEmpty(title) && isLength(title, { min: 3, max: 100 }) &&
+  isNotEmpty(body) && isLength(body, { min: 10 }) &&
   isBase64(image) &&
-  isMongoId(category)
+  isNotEmpty(category) && isMongoId(category)
 )
 
 exports.update = (id, title, body, category) => (
   typeof id === 'number' &&
-  isLength(title, { min: 3, max: 100 }) &&
-  isLength(body, { min: 10 }) &&
-  isMongoId(category)
+  isNotEmpty(title) && isLength(title, { min: 3, max: 100 }) &&
+  isNotEmpty(body) && isLength(body, { min: 10 }) &&
+  isNotEmpty(category) && isMongoId(category)
 )
 
 exports.image = (image) => image === undefined || isBase64(image)
