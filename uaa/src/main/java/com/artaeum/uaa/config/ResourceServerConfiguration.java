6bd9750ecb2951da556ad53ecbeb4ecad928da78
EgorTrevelyan <egorepnikov@gmail.com>
2019-04-21 16:40:39 +0300

    Add exceptionHandler to UAA

--- uaa/src/main/java/com/artaeum/uaa/config/ResourceServerConfiguration.java
+++ uaa/src/main/java/com/artaeum/uaa/config/ResourceServerConfiguration.java
@@ -1,11 +1,14 @@
 package com.artaeum.uaa.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 @Configuration
 @EnableResourceServer
@@ -23,6 +26,9 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
                     .sessionManagement()
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                    .exceptionHandling()
+                    .authenticationEntryPoint(authenticationEntryPoint())
+                .and()
                     .authorizeRequests()
                     .antMatchers("/actuator/**").permitAll()
                     .antMatchers("/register").permitAll()
@@ -34,5 +40,14 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
                     .antMatchers(HttpMethod.DELETE, "/users/**").hasAuthority(Constants.ADMIN_AUTHORITY)
                     .antMatchers(HttpMethod.GET, "/authorities").hasAuthority(Constants.ADMIN_AUTHORITY)
                     .antMatchers("/**").authenticated();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        return (request, response, authException) -> {
+            response.addHeader("Content-Type", "application/json");
+            response.getWriter().append("{\"message\":\"UNAUTHORIZED\"}");
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        };
     }
 }
