5aa39ae47ff18d32d11b3ca952fd63fc2a2c5759
EgorTrevelyan <egorepnikov@gmail.com>
2019-04-22 20:23:58 +0300

    Add EurekaClient to Koa context

--- comment/src/app.js
+++ comment/src/app.js
@@ -1,7 +1,6 @@
 const Koa = require('koa')
 
 const mongoConfig = require('./lib/mongo-config')
-const eureka = require('./lib/eureka-client')
 const handlers = require('./handlers')
 const controllers = require('./controllers')
 
@@ -13,9 +12,5 @@ app.use(controllers.routes())
 app.use(controllers.allowedMethods())
 
 mongoConfig()
-
-if (process.env.NODE_ENV !== 'test') {
-  eureka.start()
-}
 
 module.exports = app
--- comment/src/index.js
+++ comment/src/index.js
@@ -1,6 +1,10 @@
 const app = require('./app')
 const config = require('./lib/config')
+const eureka = require('./lib/eureka-client')
+
+app.context.eureka = eureka
 
 app.listen(config.port, () => {
+  eureka.start()
   console.log(`Server has been started on port ${config.port}`)
 })
