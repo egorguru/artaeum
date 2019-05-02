444877fe0919a4d6fdc87d1f404b52b1d912df54
EgorTrevelyan <egorepnikov@gmail.com>
2019-05-02 22:39:32 +0300

    Add mongoose auto reconnection

--- statistics/src/lib/mongo-config.js
+++ statistics/src/lib/mongo-config.js
@@ -3,7 +3,12 @@ const config = require('./config')
 
 module.exports = () => {
   mongoose
-    .connect(config.mongoUri, { useNewUrlParser: true })
+    .connect(config.mongoUri, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      reconnectTries: Number.MAX_VALUE,
+      reconnectInterval: 10000
+    })
     .then((() => console.log('MongoDB has been connected')))
     .catch((error) => console.log(error))
 }
