1f1aa705a9b51d2536b2e6375cf80ce2b924d488
EgorTrevelyan <egorepnikov@gmail.com>
2019-05-03 23:43:26 +0300

    Remove mongoose auto reconnection

--- statistics/src/lib/mongo-config.js
+++ statistics/src/lib/mongo-config.js
@@ -5,9 +5,7 @@ module.exports = () => {
   mongoose
     .connect(config.mongoUri, {
       useNewUrlParser: true,
-      useFindAndModify: false,
-      reconnectTries: Number.MAX_VALUE,
-      reconnectInterval: 10000
+      useFindAndModify: false
     })
     .then((() => console.log('MongoDB has been connected')))
     .catch((error) => console.log(error))
