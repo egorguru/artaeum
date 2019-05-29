3e8c30ac5865bfd9b4680b4cd4e9d45a22bac818
EgorTrevelyan <egorepnikov@gmail.com>
2019-05-29 20:09:17 +0300

    run-clean.sh script

---
+++ run-clean.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+rm -rf blog/node_modules; rm blog/package-lock.json;
+mvn clean -f gateway;
+rm -rf media/node_modules; rm media/package-lock.json;
+mvn clean -f profile;
+mvn clean -f registry;
+rm -rf statistics/node_modules; rm statistics/package-lock.json;
+mvn clean -f storage;
+mvn clean -f uaa;
+rm -rf web-client/node_modules; rm web-client/package-lock.json;
