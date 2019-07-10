57c13feb161f84562c5cd0ced7a27a1c0e468fb0
EgorTrevelyan <egorepnikov@gmail.com>
2019-07-10 20:11:01 +0300

    Remove Profile

--- profile/src/main/java/com/artaeum/profile/controller/error/InternalServerException.java
+++
@@ -1,7 +0,0 @@
-package com.artaeum.profile.controller.error;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-public class InternalServerException extends RuntimeException {}
