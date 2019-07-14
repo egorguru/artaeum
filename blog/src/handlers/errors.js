b597bbb3ead1e475cc8cf44dc1d9620a52ca06a8
EgorTrevelyan <egorepnikov@gmail.com>
2019-07-14 22:17:31 +0300

    log on error

--- blog/src/handlers/errors.js
+++ blog/src/handlers/errors.js
@@ -2,6 +2,7 @@ module.exports = async (ctx, next) => {
   try {
     await next()
   } catch(e) {
+    console.log(e)
     ctx.body = { error: e.message || 'Internal Server Error' }
     ctx.status = e.status || 500
   }
