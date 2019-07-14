003993fb026329c833e2d69f085befbdacd2ee67
EgorTrevelyan <egorepnikov@gmail.com>
2019-07-14 23:29:45 +0300

    Add RestTemplate bean

--- gateway/src/main/java/com/artaeum/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/artaeum/gateway/GatewayApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,5 +13,10 @@ public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
