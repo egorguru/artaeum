80676f61a4af674c38ba6913fc948afe7e2679f0
EgorTrevelyan <egorepnikov@gmail.com>
2019-07-14 04:12:40 +0300

    Update Eureka config

--- storage/src/main/scala/com/artaeum/storage/task/EurekaTask.scala
+++ storage/src/main/scala/com/artaeum/storage/task/EurekaTask.scala
@@ -11,13 +11,14 @@ class EurekaTask(context: Context) extends Task(context) {
   var eureka: EurekaService = _
 
   override def run(): Unit = {
+    val port = Config.get("port")
     this.eureka = EurekaService(
       "http://registry:8761/eureka/",
       "storage",
-      "storage-1",
+      "storage:storage:%s".format(port),
       "STORAGE",
       "127.0.0.1",
-      Config.get("port").toInt
+      port.toInt
     )
   }
 
