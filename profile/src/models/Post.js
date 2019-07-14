c689c81f90ac4c277156a6c1ba75f5811d3e7b28
EgorTrevelyan <egorepnikov@gmail.com>
2019-07-14 20:02:01 +0300

    Refactor Profile Models

--- profile/src/models/Post.js
+++ profile/src/models/Post.js
@@ -1,17 +1,23 @@
 const Sequelize = require('sequelize')
 
 module.exports = (sequelize) =>
-  sequelize.define('posts', {
+  sequelize.define('post', {
     userId: {
       type: Sequelize.STRING,
-      allowNull: false
+      allowNull: false,
+      field: 'user_id'
     },
     text: {
       type: Sequelize.STRING,
-      allowNull: false
+      allowNull: false,
+      field: 'text'
     },
     createdDate: {
       type: Sequelize.DATE,
-      defaultValue: Sequelize.NOW
+      defaultValue: Sequelize.NOW,
+      field: 'created_date'
     }
-  }, { timestamps: false })
+  }, {
+    timestamps: false,
+    tableName: 'posts'
+  })
--- profile/src/models/Subscription.js
+++ profile/src/models/Subscription.js
@@ -1,17 +1,23 @@
 const Sequelize = require('sequelize')
 
 module.exports = (sequelize) =>
-  sequelize.define('subscriptions', {
+  sequelize.define('subscription', {
     profileId: {
       type: Sequelize.STRING,
-      allowNull: false
+      allowNull: false,
+      field: 'profile_id'
     },
     subscriberId: {
       type: Sequelize.STRING,
-      allowNull: false
+      allowNull: false,
+      field: 'subscriber_id'
     },
     createdDate: {
       type: Sequelize.DATE,
-      defaultValue: Sequelize.NOW
+      defaultValue: Sequelize.NOW,
+      field: 'created_date'
     }
-  }, { timestamps: false })
+  }, {
+    timestamps: false,
+    tableName: 'subscriptions'
+  })
