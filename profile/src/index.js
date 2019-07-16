6127783a5db367e25e4cf08e00cf2b89fd75812b
EgorTrevelyan <egorepnikov@gmail.com>
2019-07-16 23:26:16 +0300

    Fix Profile

--- profile/src/index.js
+++ profile/src/index.js
@@ -4,10 +4,9 @@ const eureka = require('./lib/eureka-client')
 const config = require('./lib/config')
 const app = require('./app')
 
-eureka.start()
-
-app.addHandlerBefore((ctx) => ctx.eureka = eureka)
+app.middleware((ctx) => ctx.eureka = eureka)
 
 http.createServer(app.toListener()).listen(config.port, () => {
+  eureka.start()
   console.log(`Server has been started on port ${config.port}`)
 })
